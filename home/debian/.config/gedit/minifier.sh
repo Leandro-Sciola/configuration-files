740d9e57e31e6804e63cb26f87cb2e35d9da9081
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2023-05-30 11:15:43 -0300

    Update minifier.sh

--- home/debian/.config/gedit/minifier.sh
+++ home/debian/.config/gedit/minifier.sh
@@ -7,8 +7,13 @@ file_type() {
     done
 }
 filepath=$GEDIT_CURRENT_DOCUMENT_DIR
-cd $filepath
-cd ../dist
+cd "$filepath"
+for path in "." ".." "../.." "../../.." "../../../.." "../../../../.."; do
+    if [ -d "$path/dist" ]; then
+        cd "$path/dist"
+        break
+    fi
+done
 filename="${GEDIT_CURRENT_DOCUMENT_NAME%.*}"
 if [ $1 = "css" ]; then
     if [ -d "./css" ]; then
